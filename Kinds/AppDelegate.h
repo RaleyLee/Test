08915790c4530a16444bf18e94970ea4917435c9
hibor <hibor@mac.local>
2018-06-20 10:45:16 +0800

    Initial Commit

---
+++ Kinds/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Kinds
+//
+//  Created by hibor on 2018/6/20.
+//  Copyright © 2018年 hibor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
